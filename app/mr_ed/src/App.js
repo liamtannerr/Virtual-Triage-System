418bc700f8b29d8ce534f8af9ccdd272376fb78f
IsaacNorthrop <isaac.northrop88@gmail.com>
2024-10-19 18:01:53 -0700

    bootstrap added

--- app/mr_ed/src/App.js
+++ app/mr_ed/src/App.js
@@ -1,21 +1,22 @@
 import React from 'react';
- import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
- import Login from './components/Login';
- import Register from './components/Register';
- import Homepage from './components/Homepage';
- import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
+import Homepage from './components/Homepage';
+import EDNavbar from './components/EDNavbar';
+
 function App() {
- return (
- <Router>
- <Navbar />
- <div className="container">
- <Routes>
- <Route path="/" element={<Homepage />} />
- <Route path="/login" element={<Login />} />
- <Route path="/register" element={<Register />} />
- </Routes>
- </div>
- </Router>
- );
- }
+  return (
+    <Router>
+      <EDNavbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
 export default App;
