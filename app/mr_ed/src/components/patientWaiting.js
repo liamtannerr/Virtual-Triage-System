44708b569887946a317076ef53cf9f4b49d21b0c
Isaac Northrop <isaac.northrop88@gmail.com>
2024-11-30 16:55:18 -0800

    routing done

--- app/mr_ed/src/components/patientWaiting.js
+++ app/mr_ed/src/components/patientWaiting.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { ClipLoader } from 'react-spinners';
 
 const PatientWaiting = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (localStorage.getItem('inTriage') !== 1) {
+            navigate('/');
+        }
+    }, []);
+
     return (
         <div className="patient-waiting" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh', textAlign: 'center' }}>
             <h1>Waiting Room</h1>
